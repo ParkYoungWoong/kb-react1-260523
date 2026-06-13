bb9444309ec4bc47aef5ba5bfeac7e0b6be17c98
ParkYoungWoong <thesecon@gmail.com>
2026-06-13 10:52:12 +0900

    2026-06-13 10:52

--- src/routes/pages/MovieDetails.tsx
+++ src/routes/pages/MovieDetails.tsx
@@ -1,15 +1,65 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
 
-// 10:14
+export interface Movie {
+  Title: string
+  Year: string
+  Rated: string
+  Released: string
+  Runtime: string
+  Genre: string
+  Director: string
+  Writer: string
+  Actors: string
+  Plot: string
+  Language: string
+  Country: string
+  Awards: string
+  Poster: string
+  Ratings: Rating[]
+  Metascore: string
+  imdbRating: string
+  imdbVotes: string
+  imdbID: string
+  Type: string
+  DVD: string
+  BoxOffice: string
+  Production: string
+  Website: string
+  Response: string
+}
+export interface Rating {
+  Source: string
+  Value: string
+}
 
 // http://localhost:5173/movies/tt1234568?a=1&b=2&c=3
 export default function MovieDetails() {
   const { movieId } = useParams()
+  const [movie, setMovie] = useState<Movie | null>(null)
 
-  async function fetchMovie() {
-    const res = await fetch(`https://omdbapi.com?apikey=7035c60c&i=${movieId}`) // 'GET'
-    const movie = await res.json()
-  }
+  useEffect(() => {
+    async function fetchMovie() {
+      const res = await fetch(
+        `https://omdbapi.com?apikey=7035c60c&i=${movieId}`
+      ) // 'GET'
+      setMovie(await res.json())
+    }
+    fetchMovie()
+  }, [])
 
-  return <h1>{movieId}</h1>
+  return (
+    <>
+      {movie && (
+        <>
+          <h1>{movie.Title}</h1>
+          <p>{movie.Plot}</p>
+          <p>{movie.Actors}</p>
+          <p>{movie.Genre}</p>
+          <p>{movie.Director}</p>
+          <p>{movie.Writer}</p>
+        </>
+      )}
+    </>
+  )
 }
