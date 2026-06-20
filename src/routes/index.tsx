59361983a20f76c1568228ff92bcee5ddab9714f
ParkYoungWoong <thesecon@gmail.com>
2026-06-20 11:02:27 +0900

    26-06-20 11:02

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-import { lazy, Suspense } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Default from './layouts/Default'
 import Empty from './layouts/Empty'
@@ -10,10 +9,19 @@ import MovieDetails from './pages/MovieDetails'
 // import SignIn from './pages/SignIn'
 import { requiresAuth, guestOnly } from './loaders'
 import Loader from '@/components/Loader'
+import { dynamic } from './dynamic'
+import type { DynamicOptions } from './dynamic'
 
-const About = lazy(() => import('./pages/About'))
-const NotFound = lazy(() => import('./pages/NotFound'))
-const SignIn = lazy(() => import('./pages/SignIn'))
+const dynamicOptions: DynamicOptions = {
+  error: ({ error }) => {
+    if (error instanceof Error) return <h1>에러 발생: {error.message}</h1>
+    return <h1>에러 발생!</h1>
+  },
+  loading: <Loader size={200} />
+}
+const About = dynamic(() => import('./pages/About'), dynamicOptions)
+const NotFound = dynamic(() => import('./pages/NotFound'), dynamicOptions)
+const SignIn = dynamic(() => import('./pages/SignIn'), dynamicOptions)
 
 // https://heropy.dev/movies/1234 // ✅
 // https://heropy.dev/#/movies/1234 // ❌
@@ -26,9 +34,17 @@ const router = createBrowserRouter([
       {
         path: '/about',
         element: (
-          <Suspense fallback={<Loader />}>
-            <About />
-          </Suspense>
+          // <ErrorBoundary
+          //   fallbackRender={({ error }) => {
+          //     if (error instanceof Error)
+          //       return <h1>에러 발생: {error.message}</h1>
+          //     return <h1>에러 발생!</h1>
+          //   }}>
+          //   <Suspense fallback={<Loader size={200} />}>
+          //     <About />
+          //   </Suspense>
+          // </ErrorBoundary>
+          <About />
         )
       },
       {
--- src/routes/pages/About.tsx
+++ src/routes/pages/About.tsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 
 export default function About() {
   const [isOpen, setIsOpen] = useState(false)
+  // if (true) {
+  //   throw new Error('안녕 에러 강제 발생!!!!!!!!!!')
+  // }
   return (
     <>
       <h1>About!</h1>
