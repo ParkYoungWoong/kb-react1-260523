da841ade2b380c5574a3b8119272188ca4363f85
ParkYoungWoong <thesecon@gmail.com>
2026-06-20 13:50:24 +0900

    26-06-20 13:50

--- src/routes/pages/Todos.tsx
+++ src/routes/pages/Todos.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import TodoItem from '@/components/todos/TodoItem'
 
 interface Todo {
   id: string // 할 일 ID
@@ -10,37 +11,58 @@ interface Todo {
   updatedAt: string // 할 일 수정일
 }
 
+const todoApi = axios.create({
+  baseURL: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
+  headers: {
+    'content-type': 'application/json',
+    apikey: 'KDT8_bcAWVpD8',
+    username: 'KDT8_ParkYoungWoong'
+  }
+})
+
 export default function Todos() {
   const [todos, setTodos] = useState<Todo[]>([])
+  const [title, setTitle] = useState('')
 
   useEffect(() => {
     ;(async function () {
       // Fetch: const res = await fetch()
       //        const data = await res.json()
       // Axios: const { data } = await axios()
-      const { data } = await axios.get(
-        'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
-        {
-          headers: {
-            'content-type': 'application/json',
-            apikey: 'KDT8_bcAWVpD8',
-            username: 'KDT8_ParkYoungWoong'
-          }
-        }
-      )
+      const { data } = await todoApi.get('/')
       setTodos(data)
     })()
   }, [])
 
-  function createTodo() {
-    //
+  async function createTodo() {
+    if (!title.trim()) return
+    try {
+      const { data } = await todoApi.post<Todo>('/', { title })
+      setTodos([data, ...todos])
+      setTitle('')
+    } catch (error) {
+      if (error instanceof Error) {
+        return console.error(error.message)
+      }
+      return console.log(error)
+    }
   }
 
   return (
     <>
       <div>
-        <input type="text" />
-        <button>추가</button>
+        <input
+          type="text"
+          value={title}
+          onChange={event => {
+            setTitle(event.target.value)
+          }}
+          onKeyDown={event => {
+            if (event.nativeEvent.isComposing) return
+            if (event.key === 'Enter') createTodo()
+          }}
+        />
+        <button onClick={() => createTodo()}>추가</button>
       </div>
       <ul>
         {todos.map(todo => {
