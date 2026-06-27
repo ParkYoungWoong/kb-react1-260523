4f8d2f2416f58f07283d2a2f81f7f40984f9d5d4
ParkYoungWoong <thesecon@gmail.com>
2026-06-27 10:01:14 +0900

    26-06-27 10:01

--- src/routes/pages/About.tsx
+++ src/routes/pages/About.tsx
@@ -1,14 +1,25 @@
 import Modal from '@/components/Modal'
 import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 export default function About() {
   const [isOpen, setIsOpen] = useState(false)
+  const { data } = useQuery({
+    queryKey: ['delay'],
+    queryFn: async () => {
+      const { data } = await axios.get('https://api.heropy.dev/v0/delay?t=2000')
+      return data
+    },
+    staleTime: 1000 * 20
+  })
   // if (true) {
   //   throw new Error('안녕 에러 강제 발생!!!!!!!!!!')
   // }
   return (
     <>
       <h1>About!</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       <button onClick={() => setIsOpen(true)}>모달 열기!</button>
       {isOpen && <Modal onClose={() => setIsOpen(false)}>나는 모달!</Modal>}
     </>
--- src/routes/pages/Home.tsx
+++ src/routes/pages/Home.tsx
@@ -1,3 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
 export default function Home() {
-  return <h1>Home!</h1>
+  const { data } = useQuery({
+    queryKey: ['delay'],
+    queryFn: async () => {
+      const { data } = await axios.get('https://api.heropy.dev/v0/delay?t=2000')
+      return data
+    },
+    staleTime: 1000 * 20,
+    enabled: false
+  })
+
+  return (
+    <>
+      <h1>Home!</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  )
 }
